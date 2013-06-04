81149fb97c7201ab691b2b4bc33c5a132f7a70e3
Alex Marquez <alex@grokalex.com>
2013-06-04 13:21:04 -0400

    Fix derp and symbolize heuristic

--- src/com/android/dx/cfa2/adt/godel/PrimeFactory.scala
+++ src/com/android/dx/cfa2/adt/godel/PrimeFactory.scala
@@ -4,12 +4,13 @@ import java.math.BigInteger
 import scala.collection.mutable._
 
 object PrimeFactory {
+  val initialPrimeHeuristic = 100
   final class Prime private[PrimeFactory] (val n: BigInteger) extends AnyVal
   implicit def unwrap(p: Prime) = p.n
 }
 sealed abstract class PrimeFactory {
   import PrimeFactory._
-  protected final val memo: ArrayBuffer[BigInteger] = new ArrayBuffer[BigInteger](100)
+  protected final val memo: ArrayBuffer[BigInteger] = new ArrayBuffer[BigInteger](initialPrimeHeuristic)
   final def apply(i: Int): Prime = {
     ensure(i+1)
     new Prime(memo(i))
@@ -20,6 +21,9 @@ sealed abstract class PrimeFactory {
 object ProbablePrimeFactory extends PrimeFactory {
   def ensure(i) = {
     var cnt = memo.size
-    while(cnt < i) memo += memo.last.nextProbablePrime
+    while(cnt < i) {
+      memo += memo.last.nextProbablePrime
+      cnt -= 1
+    }
   }
 }
